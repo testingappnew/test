ec38058483709f38108554f97b8085d4dee13b7d
codemac-08i <codemac@codemac-08is-Mac-mini.local>
2018-09-15 12:49:48 +0530

    Nikki

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hi")
+        
     }
 
     override func didReceiveMemoryWarning() {
